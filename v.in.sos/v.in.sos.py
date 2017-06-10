decaba1e79cab59d75fdd93d936cd694bae4782e
pesekon2 <ondra.lobo@seznam.cz>
2017-06-10 18:48:13 +0200

    v.in.sos UI

--- v.in.sos/v.in.sos.py
+++ v.in.sos/v.in.sos.py
@@ -1,10 +1,10 @@
-#! /usr/bin/python
-
+#!/usr/bin/env python
+#
 ############################################################################
 #
 # MODULE:	    v.in.sos
 # AUTHOR(S):	Ondrej Pesek <pesej.ondrek@gmail.com>
-# PURPOSE:	    Load data from SOS server as a vector layer to GRASS GIS
+# PURPOSE:	    Import data from SOS server as a vector layer to GRASS GIS
 # COPYRIGHT:	(C) 2017 Ondrej Pesek and the GRASS Development Team
 #
 #		This program is free software under the GNU General
@@ -13,31 +13,68 @@
 #
 #############################################################################
 
-
-# %Module
-# % description: Load data from SOS server as a vector layer to GRASS GIS.
-# % keyword: vector
-# % keyword: import
-# % keyword: SOS
-# %end
-# %option
-# % key: url
-# % type: string
-# % description: Base URL starting with 'http' and ending in '?'
-# % required: yes
-# %end
-
+#%module
+#% description: Load data from SOS server as a vector layer to GRASS GIS.
+#% keyword: vector
+#% keyword: import
+#% keyword: SOS
+#%end
+#%option
+#% key: url
+#% type: string
+#% description: Base URL starting with 'http' and ending in '?'
+#% required: yes
+#%end
+#%option G_OPT_V_MAP
+#% key: output
+#% description: Name for output vector map
+#% required: yes
+#%end
+#%option
+#% key: offering
+#% type: string
+#% description: A collection of sensor used to conveniently group them up
+#% required: yes
+#%end
+#%option
+#% key: response_format
+#% type: string
+#% description: Format of data output
+#% answer: text/xml;subtype="om/1.0.0"
+#% required: yes
+#%end
+#%option
+#% key: observed_properties
+#% type: string
+#% description: The phenomena that are observed
+#% required: yes
+#%end
+#%option
+#% key: procedure
+#% type: string
+#% description: Who provide the observations
+#% required: yes
+#%end
+#%option
+#% key: version
+#% type: string
+#% description: Version of SOS server
+#% options: 1.0.0, 2.0.0
+#% answer: 1.0.0
+#%end
 
 import sys
-from subprocess import PIPE
+from grass.script import parser
 
-import grass.script as grass
-
+def cleanup():
+    pass
 
 def main():
-    print("I'm runniiiing")
+    print("I'm running")
+
+    return 0
 
 
 if __name__ == "__main__":
-    options, flags = grass.parser()
+    options, flags = parser()
     main()
